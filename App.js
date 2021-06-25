fcd5ae3a7702b01c6551957f97faa8439bb8fb60
ak465 <76836592+ak465@users.noreply.github.com>
2021-06-25 14:16:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  Image,
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+import PhonicSoundButton from './components/PhonicSound';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks:[],
+      phonicSounds:[],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'#9c8210'}
+          centerComponent={{
+            text: 'Monkey Chunky',
+            style: { color: '#fff', fontSize: 20 },
+          }}
+        />
+        <Image
+          style={styles.imgicon}
+          source={{
+            uri:'https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png',
+       
+          }}
+
+    />
+          
+
+
+      
+
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+            this.setState({ chunks: db[this.state.text].chunks });
+            this.setState({phonicSounds:db[this.state.text].phones});
+          }}>
+          <Text style={styles.buttonText}>GO</Text>
+        </TouchableOpacity>
+        <View>
+         
+         {this.state.chunks.map((item, index)=>{
+         
+         return(
+            <PhonicSoundButton wordChunk={this.state.chunks[index]} soundChunk={this.state.phonicSounds[index]}/>
+             
+             
+         );
+         })}
+        
+      </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    backgroundColor: '#b8b8b8',
+  },
+  inputBox: {
+    marginTop: 200,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  
+  imgicon: {
+    width:150,
+    height:150,
+    marginLeft:80
+  },
+  
+});
